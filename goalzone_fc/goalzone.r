21b0c2fd1a76e678dbf8468b0f719073e29143e1
xrander <olamideadu@gmail.com>
2023-08-07 01:26:40 +0200

    data cleaning and validation completed

--- goalzone_fc/goalzone.r
+++ goalzone_fc/goalzone.r
@@ -10,11 +10,30 @@ goalzone_fc <- read_csv("fitness_class_2212.csv")
 # Investigate the data frame
 
 str(goalzone_fc) # structure of the data
+# All columns except 'weight', and 'month_as_member' columns doesn't follow the data description
+#The other columns are better mutated as factors.
 
-summary(goalzone_fc)
+# Checking for unique values in columns
+goalzone_col <- colnames(goalzone_fc)[5:8] # Extract column names of interest
 
-# Checking for column with missing value
-missing_values <- is.na(goalzone_fc)
-col_with_missing <- colSums(missing_values)
-col_with_missing
-goalzone_fc[is.na(goalzone_fc$weight),]
+lapply(goalzone_fc[,goalzone_col], unique) # Check for unique values
+
+
+# Mutate columns that do not follow data description to write data type
+goalzone_fc <- goalzone_fc %>%
+  mutate(booking_id = factor(booking_id),
+         days_before = factor(days_before),
+         day_of_week = factor(ifelse(day_of_week == "Wednesday", "Wed",
+                              ifelse(day_of_week == "Fri.", "Fri",
+                                     ifelse(day_of_week == "Monday", "Mon", day_of_week)))),
+         time = factor(time, levels = c("AM", "PM"),ordered = T),
+         category = factor(ifelse(category == "-", "Unknown", category)),
+         attended = factor(attended, levels = c(1,0)))
+
+summary(goalzone_fc) # quick summary statistics of the data
+# weight is having 20 missing values
+
+#replacing na in weight
+goalzone_fc <- goalzone_fc %>%
+  mutate(weight = replace_na(weight, mean(weight, na.rm = T)))
+
